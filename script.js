210017ab21dd998f3c8abc21b5097fa5799f7cc6
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-09 10:57:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
+const menu = document.getElementById("menu");
+
 btn.addEventListener("click", () => {
   btn.classList.toggle("open");
   nav.classList.toggle("flex");
   nav.classList.toggle("hidden");
 });
+
+const closeMenu = function () {
+  menu.classList.add("hidden");
+};
+// const btnCloseMenu = document.querySelector(".close-modal");
+menu.addEventListener("click", closeMenu);
